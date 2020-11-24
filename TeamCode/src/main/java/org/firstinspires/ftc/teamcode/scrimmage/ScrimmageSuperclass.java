b65040d8be150bf3a818843010fc8a58a3b4bca3
Allen Sarmiento <allenbsarmiento01@gmail.com>
2020-11-24 15:11:02 -0500

    Implemented Vision Code from Template
    - Created VisionTemplate Class
    - Transferred Vision Code into ScrimmageSuperclass

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrimmage/ScrimmageSuperclass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrimmage/ScrimmageSuperclass.java
@@ -5,10 +5,21 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
 
 public abstract class ScrimmageSuperclass extends LinearOpMode {
 
@@ -21,6 +32,42 @@ public abstract class ScrimmageSuperclass extends LinearOpMode {
     public BNO055IMU imu;
     public Orientation orientation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);;
     public float angle = orientation.thirdAngle; // temp
+
+    // Vuforia
+    // IMPORTANT: If you are using a USB WebCam, camera choice "BACK" and phone portrait "false"
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+    private static final boolean PHONE_IS_PORTRAIT = false;
+
+    private static final String VUFORIA_KEY = "AaMuRa7/////AAABmeeXefeDrEfGtTjiMIEuO2djgL8Uz6M9/NrJ" +
+            "CrNousZ9V7tnau7MP3q5eACYGf+HgjNwjsOkV8ERj5yJglYfVjm3W9NBeAEAP18/1TMnFvSY6+dalmccEnnbag" +
+            "eBAPAVMBLk5OLCA35uka2sjuLb37/rdMPNJGmSqklqcthb1NuxWzpVe7BZcf2YODtUPWnTHKi5t5s6XKQA5p4T" +
+            "u6x73Mha8a6jN7hv/pnvneUoG0N5Eih6gZ1sSXKcGfpqjf1npkJUb4AcMoqYE0DE31kUk+V/N2hjNsg9mQSGw2" +
+            "TmXG7Iq15ugKdyFwzgpWf6IueyoTKkwOczEiGALV2lObz+fyFLob4rq6HtpkCpL4gkh4xy";
+
+    // Class Members
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * This is the webcam we are to use. As with other hardware devices such as motors and
+     * servos, this device is identified using the robot configuration tool in the FTC application.
+     */
+
+    WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+    private boolean targetVisible;
+
+    /*
+     * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+     * We can pass Vuforia the handle to a camera preview resource (on the RC phone);
+     * If no camera monitor is desired, use the parameter-less constructor instead (commented out below).
+     */
+    int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+    VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+    // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+    VuforiaTrackables targetsUltimateGoal;
+
+    List<VuforiaTrackable> allTrackables;
 
     // CONTROL CONSTANTS ---------------------------------------------------------------------------
 
@@ -59,6 +106,40 @@ public abstract class ScrimmageSuperclass extends LinearOpMode {
         // Telemetry
         telemetry.addLine("Robot Initialized");
         telemetry.update();
+    }
+
+    public void initializeVuforia() {
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+
+        /**
+         * We also indicate which camera on the RC we wish to use.
+         */
+        parameters.cameraName = webcamName;
+
+        // Make sure extended tracking is disabled for this example.
+        parameters.useExtendedTracking = false;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Load the data sets for the trackable objects. These particular data
+        // sets are stored in the 'assets' part of our application.
+        targetsUltimateGoal = this.vuforia.loadTrackablesFromAsset("UltimateGoal");
+        VuforiaTrackable blueTowerGoalTarget = targetsUltimateGoal.get(0);
+        blueTowerGoalTarget.setName("Blue Tower Goal Target");
+        VuforiaTrackable redTowerGoalTarget = targetsUltimateGoal.get(1);
+        redTowerGoalTarget.setName("Red Tower Goal Target");
+        VuforiaTrackable redAllianceTarget = targetsUltimateGoal.get(2);
+        redAllianceTarget.setName("Red Alliance Target");
+        VuforiaTrackable blueAllianceTarget = targetsUltimateGoal.get(3);
+        blueAllianceTarget.setName("Blue Alliance Target");
+        VuforiaTrackable frontWallTarget = targetsUltimateGoal.get(4);
+        frontWallTarget.setName("Front Wall Target");
+
+        // For convenience, gather together all the trackable objects in one easily-iterable collection */
+        allTrackables = new ArrayList<VuforiaTrackable>();
+        allTrackables.addAll(targetsUltimateGoal);
     }
 
     // Drive Methods
@@ -186,6 +267,35 @@ public abstract class ScrimmageSuperclass extends LinearOpMode {
             setDrivePower(0);
             resetDriveMode();
         }
+    }
+
+    // Vision Methods
+    public void vuforiaScan() {
+
+        // Note: To use the remote camera preview:
+        // AFTER you hit Init on the Driver Station, use the "options menu" to select "Camera Stream"
+        // Tap the preview window to receive a fresh image.
+
+        targetsUltimateGoal.activate();
+
+        // Change condition to something else later
+        while (!isStopRequested()) {
+
+            // check all the trackable targets to see which one (if any) is visible.
+            targetVisible = false;
+            for (VuforiaTrackable trackable : allTrackables) {
+                if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
+                    telemetry.addData("Visible Target", trackable.getName());
+                    targetVisible = true;
+                    break;
+                }
+            }
+
+            telemetry.update();
+        }
+
+        // Disable Tracking when we are done;
+        targetsUltimateGoal.deactivate();
     }
 
     // UTILITY METHODS -----------------------------------------------------------------------------
